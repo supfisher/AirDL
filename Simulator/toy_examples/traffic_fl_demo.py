79942fae62b49e8148741d22a679ed3f860793c7
chuanting <chuanting.zhang@kaust.edu.sa>
2020-02-29 18:33:32 +0300

    update traffic demo

--- Simulator/toy_examples/traffic_fl_demo.py
+++ Simulator/toy_examples/traffic_fl_demo.py
@@ -1,40 +1,37 @@
 # -*- coding: utf-8 -*-
 """
 -----------------------------------------------
-# File: mnist_fl_demo.py
+# File: traffic_fl_demo.py
 # This file is created by Chuanting Zhang
 # Email: chuanting.zhang@kaust.edu.sa
-# Date: 2020-02-28 (YYYY-MM-DD)
+# Date: 2020-02-29 (YYYY-MM-DD)
 -----------------------------------------------
 """
+
 from network import *
 import yaml
 from models import *
-from utils import *
+from utils.dataloader_tmp import DataParallel
 import argparse
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.optim.lr_scheduler import StepLR
-from time import time
 import numpy as np
 import pandas as pd
+import h5py
+import random
+from sklearn import metrics
 from collections import deque
 
-parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser = argparse.ArgumentParser(description='Wireless traffic prediction example')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=50, metavar='N',
+parser.add_argument('--epochs', type=int, default=100, metavar='N',
                     help='number of epochs to train (default: 14)')
-parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 1.0)')
-parser.add_argument('--num_channels', type=int, default=1, help='input channels')
-parser.add_argument('--num_classes', type=int, default=10, help='number of classes')
-parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
-                    help='Learning rate step gamma (default: 0.7)')
 parser.add_argument('--no-cuda', action='store_true', default=True,
                     help='disables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
@@ -46,17 +43,101 @@ parser.add_argument('--save-model', action='store_true', default=False,
 
 parser.add_argument('--dist_url', default='tcp://127.0.0.1:8001', type=str,
                     help='For Saving the current Model')
-parser.add_argument('--rank', default=0, type=int,
+parser.add_argument('--rank', default=1, type=int,
                     help="Currently, we only support the backend of mpi and gloo,You don't need "
                          "to care about it if using mpi. However, you have to assert it to be 0"
                          "on your master process.")
-parser.add_argument('--world_size', default=0, type=int,
+parser.add_argument('--world_size', default=2, type=int,
                     help="The total number of processes.")
 parser.add_argument('--clients', type=int, default=10, help='number of clients')
-parser.add_argument('--epsilon', type=float, default=0.1, help='time window')
+parser.add_argument('--epsilon', type=float, default=1.0, help='decay threshold')
 parser.add_argument('--mode', type=str, default='wireless', help='channel mode')
 parser.add_argument('--stop', type=bool, default=False, help='set a stop condition or not')
 parser.add_argument('--condition', type=float, default=95, help='stop condition value')
+
+parser.add_argument('--close_size', type=int, default=3,
+                    help='how many time slots before target are used to model closeness')
+parser.add_argument('--test_days', type=int, default=7,
+                    help='how many days data are used to test model performance')
+parser.add_argument('--val_days', type=int, default=0,
+                    help='how many days data are used to valid model performance')
+
+# FL Neural Network parameters
+parser.add_argument('--input_dim', type=int, default=1,
+                    help='input feature dimension of LSTM')
+parser.add_argument('--hidden_dim', type=int, default=64,
+                    help='hidden neurons of LSTM layer')
+parser.add_argument('--num_layers', type=int, default=2,
+                    help='number of layers of LSTM')
+parser.add_argument('--out_dim', type=int, default=1,
+                    help='how many steps we would like to predict for the future')
+
+
+def get_data(args):
+    f = h5py.File('D:\Projects\FedMMoE\scripts\dataset/milano.h5', 'r')
+
+    idx = f['idx'][()]
+    cell = f['cell'][()]
+    data = f['net'][()][:, cell - 1]
+
+    df = pd.DataFrame(data, index=pd.to_datetime(idx.ravel(), unit='s'), columns=cell)
+    df.fillna(0, inplace=True)
+
+    random.seed(args.seed)
+    selected_cells = sorted(random.sample(list(cell), args.clients))
+    # print('Selected cells:', selected_cells)
+
+    df_cells = df[selected_cells]
+    # print(df_cells.head())
+
+    train_data = df_cells.iloc[:-7 * 24]
+    mean = train_data.mean()
+    std = train_data.std()
+
+    normalized_df = (df_cells - mean) / std
+    # print(normalized_df.head())
+
+    return normalized_df, df_cells, selected_cells, mean, std
+
+
+def process_isolated(args, dataset):
+    train, val, test = dict(), dict(), dict()
+    column_names = dataset.columns
+
+    for col in column_names:
+        close_arr, period_arr, label_arr = [], [], []
+
+        cell_traffic = dataset[col]
+        start_idx = args.close_size
+        for idx in range(start_idx, len(dataset) - args.out_dim + 1):
+            y_ = [cell_traffic.iloc[idx + i] for i in range(args.out_dim)]
+            label_arr.append(y_)
+
+            if args.close_size > 0:
+                x_close = [cell_traffic.iloc[idx - c] for c in range(1, args.close_size + 1)]
+                close_arr.append(x_close)
+
+        cell_arr_close = np.array(close_arr)
+        cell_arr_close = cell_arr_close[:, :, np.newaxis]
+        cell_label = np.array(label_arr)
+
+        test_len = args.test_days * 24
+        val_len = args.val_days * 24
+        train_len = len(cell_arr_close) - test_len - val_len
+
+        train_x_close = cell_arr_close[:train_len]
+        val_x_close = cell_arr_close[train_len:train_len + val_len]
+        test_x_close = cell_arr_close[-test_len:]
+
+        train_label = cell_label[:train_len]
+        val_label = cell_label[train_len:train_len + val_len]
+        test_label = cell_label[-test_len:]
+
+        train[col] = (train_x_close, train_label)
+        val[col] = (val_x_close, val_label)
+        test[col] = (test_x_close, test_label)
+
+    return train, val, test
 
 
 def train(args, model, criterion, device, train_loader, optimizer, epoch):
@@ -68,7 +149,7 @@ def train(args, model, criterion, device, train_loader, optimizer, epoch):
     batch_loss = []
 
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+        data, target = data.float().to(device), target.float().to(device)
         optimizer.zero_grad()
         output = model(data)
         if output is not None:
@@ -76,8 +157,6 @@ def train(args, model, criterion, device, train_loader, optimizer, epoch):
             loss.backward()
             optimizer.step()
             batch_loss.append(loss.item())
-            # print(loss.item())
-            # print("report: ", args.topo.report)
 
     avg_loss = np.array(batch_loss).mean(axis=0)
     consume_energy = args.topo.report.energy_cost
@@ -86,7 +165,7 @@ def train(args, model, criterion, device, train_loader, optimizer, epoch):
     packet_loss = args.topo.report.packet_loss
     print('Train Epoch: {} [{}/{} ({:.2f})%], unstable up-links: {}, down-links: {}, '
           'decay threshold: {:.4f}, Avg loss: {:.4f}, '
-          'Consumed energy: {:.4f}, Used time: {:.4f}, '
+          'Consumed energy: {:.4f}, Used time: {:.4f},'
           'Goodput: {:.4f}, Packet loss: {:.4f}'.format(epoch, epoch,
                                                         args.epochs,
                                                         100. * epoch / args.epochs,
@@ -95,33 +174,34 @@ def train(args, model, criterion, device, train_loader, optimizer, epoch):
                                                         args.epsilon,
                                                         avg_loss.mean(axis=0),
                                                         consume_energy,
-                                                        used_time, goodput,
-                                                        packet_loss))
+                                                        used_time, goodput, packet_loss))
     return avg_loss, down_links, up_links, consume_energy, used_time, goodput, packet_loss
 
 
 def test(args, model, criterion, device, test_loader, epoch):
     model.eval()
-    test_loss = []
-    correct = 0
+    batch_loss = []
+
+    pred_list, truth_list = [], []
 
     with torch.no_grad():
         for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
+            data, target = data.float().to(device), target.float().to(device)
             output = model(data)
             loss = criterion(output, target).item()
-            test_loss.append(sum(loss) / len(loss))  # sum up batch loss
-            out = torch.cat(output, dim=0)
-            pred = out.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            batch_loss.append(loss)
+            pred_list.append([o.numpy().ravel() for o in output])
+            truth_list.append([t.numpy().ravel() for t in target])
 
-    val_loss = sum(test_loss) / len(test_loss)
-    val_acc = 100. * correct / len(test_loader.dataset)
-
-    print('Test Epoch: {}, epsilon threshold: {:.4f}, Avg loss: {:.4f}, Accuracy: {:.2f}%'.format(
-        epoch, args.epsilon, val_loss, val_acc
+    final_pred = np.concatenate(pred_list).ravel()
+    final_truth = np.concatenate(truth_list).ravel()
+    avg_loss = np.array(batch_loss).mean(axis=0)
+    mse = metrics.mean_squared_error(final_truth, final_pred)
+    mae = metrics.mean_absolute_error(final_truth, final_pred)
+    print('Test Epoch: {}, epsilon threshold: {:.4f}, Avg loss: {:.4f}, MSE: {:.4f}, MAE: {:.4f}'.format(
+        epoch, args.epsilon, avg_loss.mean(axis=0), mse, mae
     ))
-    return val_loss, val_acc
+    return avg_loss, mse, mae
 
 
 def main():
@@ -134,19 +214,9 @@ def main():
     random.seed(args.seed)
     np.random.seed(args.seed)
 
-    # device = torch.device("cuda" if use_cuda else "cpu")
-    device = torch.device("cpu")
+    device = torch.device("cuda" if use_cuda else "cpu")
 
-    # model = Net().to(device)
-    model = CNNMnist(args).to(device)
-
-    print(args.batch_size)
-
-    # topo = Topo(model)
-    # with open('./data/simple_graph.yaml', 'r') as f:
-    #     dict = yaml.load(f)
-    # topo.load_from_dict(dict)
-
+    model = LSTM(args).to(device)
     """
         You need to specify a backend: 'mpi', 'gloo' or None
         For using 'mpi', you have to re-complie Pytorch.
@@ -159,35 +229,30 @@ def main():
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
-    train_loader = DataParallel(
-        datasets.MNIST('./data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])), topo=topo,
-        batch_size=args.batch_size, shuffle=True, **kwargs)
-    test_loader = DataParallel(
-        datasets.MNIST('./data', train=False, transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])), topo=topo,
-        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+    data, df_ori, selected_cells, mean, std = get_data(args)
+    selected_cells = data.columns
+    train_data, val_data, test_data = process_isolated(args, data)
+
+    train_loader = DataParallel(train_data, topo=topo,
+                                batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = DataParallel(test_data, topo=topo,
+                               batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
     qos = QoSDemo(topo, args)
     model_p = ModelParallel(qos=qos)
     print(model_p.qos.channel.epsilon)
     optimizer = OptimizerParallel(optim.SGD, model_p.parameters(), lr=args.lr)
-    criterion = CriterionParallel(F.nll_loss, topo=topo)
+    criterion = CriterionParallel(F.mse_loss, topo=topo)
 
     # scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     train_loss_history, d_link_history, u_link_history = [], [], []
     test_loss_history, test_acc, test_auc = [], [], []
     exp_results = []
     queue_len = 5
-    test_acc_deque = deque([90.0]*queue_len, maxlen=queue_len)
+    test_mse_deque = deque([0.5] * queue_len, maxlen=queue_len)
     for epoch in range(1, args.epochs + 1):
         # Training the model for one epoch and get the results
-        train_loss, d_link, u_link, consumed_energy, used_time, goodput, packe_tloss = train(args, model_p,
+        train_loss, d_link, u_link, consumed_energy, used_time, goodput, packet_loss = train(args, model_p,
                                                                                              criterion, device,
                                                                                              train_loader,
                                                                                              optimizer, epoch)
@@ -198,19 +263,18 @@ def main():
 
         # Test model performance
         avg_model = AvgParallel(model_p.aggregate(), args)
-        test_loss, acc = test(args, avg_model, criterion, device, test_loader, epoch)
-        test_acc_deque.append(acc)
-
+        test_loss, mse, mae = test(args, avg_model, criterion, device, test_loader, epoch)
+        test_mse_deque.append(mse)
         test_loss_history.append(test_loss)
-        # test_acc.append(acc)
-        exp_results.append((args.epsilon, d_link, u_link, acc, consumed_energy, used_time, goodput, packe_tloss))
+        # # test_acc.append(acc)
+        exp_results.append((args.epsilon, d_link, u_link, mse, mae, consumed_energy, used_time, goodput, packet_loss))
 
-        if args.stop & (np.mean(test_acc_deque) > args.condition):
+        if args.stop & (np.mean(test_mse_deque) < args.condition):
             break
 
-    df_exp = pd.DataFrame(exp_results, columns=['epsilon', 'down_link',
-                                                'up_link', 'test_acc', 'energy', 'time', 'goodput', 'packet_loss'])
-    file_name = './data/mnist_exp_epsilon={}_clients={}_channel={}'.format(args.epsilon, args.clients, args.mode)
+    df_exp = pd.DataFrame(exp_results, columns=['epsilon', 'down_link', 'up_link', 'test_mse',
+                                                'test_mae', 'energy', 'time', 'goodput', 'packet_loss'])
+    file_name = './data/traffic_exp_epsilon={}_clients={}'.format(args.epsilon, args.clients)
     df_exp.to_csv(file_name + '_acc.csv', index=False, float_format='%.4f')
 
     df_train_loss = pd.DataFrame(train_loss_history)
@@ -220,7 +284,7 @@ def main():
     df_test_loss.to_csv(file_name + '_test_loss.csv', index=False, float_format='%.4f')
 
     if args.save_model:
-        torch.save(model_p.state_dict(), "mnist_cnn.pt")
+        torch.save(model_p.state_dict(), "traffic_lstm.pt")
 
 
 if __name__ == "__main__":
